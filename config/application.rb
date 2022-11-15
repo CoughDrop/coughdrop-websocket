8cbbca3f69ed8247897b8f579d134fb8b1aadb2c
Brian Whitmer <brian.whitmer@gmail.com>
2022-11-15 16:43:39 -0700

    disable db completely

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,11 @@
 require_relative 'boot'
 
-require 'rails/all'
+require 'action_mailer/railtie'
+require 'action_controller/railtie'
+require 'action_view/railtie'
+require 'action_cable/engine'
+require 'active_storage/engine'
+require 'sprockets/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
